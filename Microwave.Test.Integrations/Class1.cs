77185afe8aa6d24b22b6eb6a17daa938f4326325
Mikkel Steen <mikkel-steen@hotmail.com>
2019-04-23 20:45:02 +0200

    added new csproj, suspecting nameing conflict by .net standard

---
+++ Microwave.Test.Integrations/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integrations
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void YesAndYes()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
