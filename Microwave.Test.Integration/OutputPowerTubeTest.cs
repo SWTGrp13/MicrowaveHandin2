bbf253665f73d848d5d943b903b0c959447cead5
Mikkel Steen <mikkel-steen@hotmail.com>
2019-04-23 19:39:33 +0200

    added test case for Jenkins Setup check

--- Microwave.Test.Integration/OutputPowerTubeTest.cs
+++ Microwave.Test.Integration/OutputPowerTubeTest.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     public class OutputPowerTubeTest
     {
-
+        [Test]
+        public void TestForJenkins() {
+            Assert.That(true, Is.EqualTo(true));
+        }
     }
 }
