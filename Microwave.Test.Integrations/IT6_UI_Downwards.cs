ab1721e0e76c2f760d31df6b162376f6d9e510d0
Mikkel Steen <mikkel-steen@hotmail.com>
2019-04-25 20:33:33 +0200

    fixed dependecy error for 2 test, found yet another dep issue

--- Microwave.Test.Integrations/IT6_UI_Downwards.cs
+++ Microwave.Test.Integrations/IT6_UI_Downwards.cs
@@ -45,13 +45,16 @@ namespace Microwave.Test.Integrations
             _buttonStartCancel = Substitute.For<IButton>();
             _door = Substitute.For<IDoor>();
 
-            _cookController = new CookController(_timer,_display,_powerTube,_uut);
+           
             _powerTube = new PowerTube(_output);
             _timer = new Timer();
             _light = new Light(_output);
             _display = new Display(_output);
             _uut = new UserInterface(_buttonPower, _buttonTime,
-                _buttonStartCancel, _door, _display, _light, _cookController);
+            // send pointer to cookController
+            _buttonStartCancel, _door, _display, _light, _cookController);
+            // update cookController instance pointer
+            _cookController = new CookController(_timer, _display, _powerTube, _uut);
         }
 
         #endregion
@@ -71,7 +74,7 @@ namespace Microwave.Test.Integrations
 
             //Assert Powerlevel Output 50
         }
-
+      
         [TestCase(1)]
         [TestCase(5)]
         [TestCase(10)]
@@ -85,15 +88,46 @@ namespace Microwave.Test.Integrations
             { 
                 _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
             }
-
-
-            _uut.OnPowerPressed(this, EventArgs.Empty);
+            
+            //_uut.OnPowerPressed(this, EventArgs.Empty); <- ikke nødvendigt da eventet bliver nedarvet
             _output.Received().OutputLine(Arg.Is<string>(str =>
                 str.Contains($"Display shows: 50 W")));
 
             //Assert Powerlevel output X*50
         }
 
+        [Test]
+        public void OnTimePressed_Ready_OutPuts()
+        {
+            //Default state is READY
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
 
+            _output.Received().OutputLine(Arg.Is<string>(str =>
+                str.Contains($"Display shows: 50 W")));
+           
+            _buttonTime.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // test for default value
+            _output.Received().OutputLine(Arg.Is<string>(str =>
+                str.Contains($"Display shows: 01:00")));
+        }
+
+        [Test]
+        public void OnStartCancel_OutPuts()
+        {
+            //Default state is READY
+            _buttonPower.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // set time
+            _buttonTime.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // output covered in previous tests
+            // _output.ClearReceivedCalls();
+            // circular dependency, ah shit. (found one error)
+            // _buttonStartCancel.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+
+            _output.Received().OutputLine(Arg.Is<string>(str =>
+                str.Contains($"Display shows: 01:00")));
+
+
+        }
     }
 }
